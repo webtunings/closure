1ff34ec5c8b5d43d5e4af36491ea63056fb0a45c
webtunings <webtunings@gmail.com>
2014-07-01 02:49:42 +0530

    code

--- 1.3 Binding.js
+++ 1.3 Binding.js
@@ -1,3 +1,17 @@
-/**
- * Created by pk on 1/7/14.
- */
+//Listing 1.9 - Call method
+
+//This is a function that returns the circumference of a circle
+var circumference = function(){
+    return 2*Math.PI*this.radius;
+};
+//define circle objects
+var circle1 = {x:100,y:200,radius:50};
+var circle2 = {x:200,y:300,radius:25};
+
+//invoke the function
+console.log(circumference.call(circle1)); //314.159
+console.log(circumference.call(circle2)); //157.079
+
+
+//--------------------------------------------------------------------------------
+//Listing 1.10 - Call with arguments
