f3f082001b9158498dfffe45ca29a3d1359ad8ee
webtunings <webtunings@gmail.com>
2014-07-01 01:08:44 +0530

    first class

--- FirstClass.js
+++ FirstClass.js
@@ -1,19 +1,26 @@
-//Assign function to a variable
+//Listing 1.1 - Assign function to a variable
 
-//circle is an object
+//This is a function that returns the circumference of a circle
 var circumference = function(circle){
     return 2*Math.PI*circle.radius;
 };
-
+//define circle object
 var circle = {x:100,y:200,radius:50};
 
+//invoke the function
 console.log(circumference(circle)); //314.159
 
 
 //--------------------------------------------------------
-//Passing function as arguments to other functions
-//JSON replacer function - don't stringify if property name is radius
+//Listing 1.2 - Passing function as arguments to other functions
+
+/*
+This is a JSON replacer function, which filters out property
+"radius" from object serialization process
+*/
+
 var filter = function(key,value){
+//don't serialize property "radius"
     if(key === "radius"){
         return undefined;
     }
@@ -22,23 +29,32 @@ var filter = function(key,value){
     }
 };
 
+//define circle object
 var circle = {x:100,y:200,radius:50};
+
+//get string version of circle object
 var info = JSON.stringify(circle,filter);
+
+//print the string equivalent of object
 console.log(info);  // '{"x":100,"y":200}'
-console.log(typeof info); 'string'
 
 
 //--------------------------------------------------------
-//Returning them as the values from other function
+//Listing 1.3 - Returning function as the value from other function
 
+//define outer function that returns inner function
 function outer(x){
+//this secret is a closure - Wait for chapter2
     var secret = 5;
     return function(){
         console.log(secret + x);
     }
 }
 
+//get the inner function
 var inner = outer(10);
+
+//invoke inner function
 inner(); //15
 
 
