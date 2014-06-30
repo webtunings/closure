fd2a9f3ed8a222d39c42f0db28ccbe309b678c20
webtunings <webtunings@gmail.com>
2014-07-01 02:24:18 +0530

    code

--- 1.2 ScopeChain.js
+++ 1.2 ScopeChain.js
@@ -76,7 +76,7 @@ console.log(g); //1000
 
 
 //--------------------------------------------------------------------------------
-//Listing 1.5 - Scope Test3
+//Listing 1.6 - Scope Test3
 var g = 1000;
 function scopeTest3(){
     console.log(a); //undefined
@@ -115,7 +115,71 @@ function scopeTest3(){
     //console.log(d); ReferenceError
     console.log(g); //1
     console.log(typeof inner === "function"); //true
-    function inner(){ var d =400;
+    function inner(){
+        var d =400;
+        console.log(a); //100
+        console.log(b); //200
+        console.log(c); //300
+        console.log(d); //400
+        console.log(g); //1
+        console.log(typeof inner === "function"); //true
+    }
+    inner();
+}
+
+scopeTest3();
+//console.log(a); ReferenceError
+//console.log(b); ReferenceError
+//console.log(c); ReferenceError
+//console.log(d); ReferenceError
+console.log(g); //1000
+console.log(typeof inner === "function"); //false
+
+
+
+//-------------------------------------------------------------------------------
+
+//Listing 1.7 - Scope Test3
+var g = 1000;
+function scopeTest3(){
+    console.log(a); //undefined
+    console.log(b); //undefined
+    console.log(c); //undefined
+    //console.log(d); ReferenceError
+    console.log(g); //undefined
+    console.log(typeof inner === "function"); //false
+    var a = 100;
+    console.log(a); // 100
+    console.log(b); //undefined
+    console.log(c); //undefined
+    //console.log(d); ReferenceError
+    console.log(g); //undefined
+    console.log(typeof inner === "function"); //false
+    if(a === 100){
+        console.log(a); //100
+        console.log(b); //undefined
+        console.log(c); //undefined
+        //console.log(d); ReferenceError
+        console.log(g); //undefined
+        console.log(typeof inner === "function"); //false
+        var b = 200;
+        console.log(a); //100
+        console.log(b); //200
+        console.log(c); //undefined
+        //console.log(d); ReferenceError
+        console.log(g); //undefined
+        console.log(typeof inner === "function"); //false
+    }
+    var g = 1;
+    var c = 300;
+    console.log(a); //100
+    console.log(b); //200
+    console.log(c); //300
+    //console.log(d); ReferenceError
+    console.log(g); //1
+    console.log(typeof inner === "function"); //false
+    var inner = function (){
+        var d =400;
         console.log(a); //100
         console.log(b); //200
         console.log(c); //300
