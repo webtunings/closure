8bea97a6a0d3638d5c407eec6c191cce271b2d86
webtunings <webtunings@gmail.com>
2014-07-01 00:50:50 +0530

    first class

--- FirstClass.js
+++ FirstClass.js
@@ -3,8 +3,34 @@
 //circle is an object
 var circumference = function(circle){
     return 2*Math.PI*circle.radius;
-}
+};
 
 var circle = {x:100,y:200,radius:50};
 
 console.log(circumference(circle)); //314.159
+
+
+//--------------------------------------------------------
+//Passing function as arguments to other functions
+//JSON replacer function - don't stringify if property name is radius
+var filter = function(key,value){
+    if(key === "radius"){
+        return undefined;
+    }
+    else{
+        return value;
+    }
+};
+
+var circle = {x:100,y:200,radius:50};
+var info = JSON.stringify(circle,filter);
+console.log(info);  // '{"x":100,"y":200}'
+console.log(typeof info); 'string'
+
+
+//--------------------------------------------------------
+//Returning them as the values from other function
+
+
+
+
