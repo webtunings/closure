699bd5a40117cc34dbb14ea11fed0481def9b3c1
webtunings <webtunings@gmail.com>
2014-07-01 02:19:19 +0530

    code

--- 1.2 ScopeChain.js
+++ 1.2 ScopeChain.js
@@ -38,7 +38,7 @@ console.log(g); //1000
 
 //---------------------------------------------------------------------------------
 //Listing 1.5 - Scope Test2
-
+var g = 1000;
 function scopeTest2(){
     console.log(a); //undefined
     console.log(b); //undefined
@@ -76,3 +76,60 @@ console.log(g); //1000
 
 
 //--------------------------------------------------------------------------------
+//Listing 1.5 - Scope Test3
+var g = 1000;
+function scopeTest3(){
+    console.log(a); //undefined
+    console.log(b); //undefined
+    console.log(c); //undefined
+    //console.log(d); ReferenceError
+    console.log(g); //undefined
+    console.log(typeof inner === "function"); //true
+    var a = 100;
+    console.log(a); // 100
+    console.log(b); //undefined
+    console.log(c); //undefined
+    //console.log(d); ReferenceError
+    console.log(g); //undefined
+    console.log(typeof inner === "function"); //true
+    if(a === 100){
+        console.log(a); //100
+        console.log(b); //undefined
+        console.log(c); //undefined
+        //console.log(d); ReferenceError
+        console.log(g); //undefined
+        console.log(typeof inner === "function"); //true
+        var b = 200;
+        console.log(a); //100
+        console.log(b); //200
+        console.log(c); //undefined
+        //console.log(d); ReferenceError
+        console.log(g); //undefined
+        console.log(typeof inner === "function"); //true
+    }
+    var g = 1;
+    var c = 300;
+    console.log(a); //100
+    console.log(b); //200
+    console.log(c); //300
+    //console.log(d); ReferenceError
+    console.log(g); //1
+    console.log(typeof inner === "function"); //true
+    function inner(){ var d =400;
+        console.log(a); //100
+        console.log(b); //200
+        console.log(c); //300
+        console.log(d); //400
+        console.log(g); //1
+        console.log(typeof inner === "function"); //true
+    }
+    inner();
+}
+
+scopeTest3();
+//console.log(a); ReferenceError
+//console.log(b); ReferenceError
+//console.log(c); ReferenceError
+//console.log(d); ReferenceError
+console.log(g); //1000
+console.log(typeof inner === "function"); //false
