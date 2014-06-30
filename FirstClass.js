bd2edd2487d43a7944391847728535ec2c478dd8
webtunings <webtunings@gmail.com>
2014-07-01 00:21:15 +0530

    first class

--- FirstClass.js
+++ FirstClass.js
@@ -1,3 +1,10 @@
-/**
- * Created by pk on 30/6/14.
- */
+//Assign function to a variable
+
+//circle is an object
+var circumference = function(circle){
+    return 2*Math.PI*circle.radius;
+}
+
+var circle = {x:100,y:200,radius:50};
+
+console.log(circumference(circle)); //314.159
