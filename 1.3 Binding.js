a0826dce6c4c2bc02d518720c7cf9384fdae6299
webtunings <webtunings@gmail.com>
2014-07-01 22:03:00 +0530

    code

--- 1.3 Binding.js
+++ 1.3 Binding.js
@@ -14,4 +14,23 @@ console.log(circumference.call(circle2)); //157.079
 
 
 //--------------------------------------------------------------------------------
-//Listing 1.10 - Call with arguments
+//Listing 1.10 - call & apply with arguments
+var makeNonEnumerable = function(){
+    for(var i=0; i < arguments.length; i++){
+        Object.defineProperty(this,arguments[i],{enumerable:false});
+    }
+};
+
+var testObject1 = {x:1,y:2,z:3};
+
+makeNonEnumerable.call(testObject1,"x","y");
+console.log(Object.getOwnPropertyDescriptor(testObject1,"x").enumerable); //false
+console.log(Object.getOwnPropertyDescriptor(testObject1,"y").enumerable); //false
+console.log(Object.getOwnPropertyDescriptor(testObject1,"z").enumerable); //true
+
+var testObject2 = {p:1,q:2,r:3};
+makeNonEnumerable.apply(testObject2,["p","q"]);
+console.log(Object.getOwnPropertyDescriptor(testObject2,"p").enumerable); //false
+console.log(Object.getOwnPropertyDescriptor(testObject2,"q").enumerable); //false
+console.log(Object.getOwnPropertyDescriptor(testObject2,"r").enumerable); //true
+
