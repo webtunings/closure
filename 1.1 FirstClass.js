7eb2180a2420eca7beac02e65145fead6ebdb745
webtunings <webtunings@gmail.com>
2014-07-02 18:20:27 +0530

    code

--- 1.1 FirstClass.js
+++ 1.1 FirstClass.js
@@ -1,11 +1,11 @@
 //Listing 1.1 - Assign function to a variable
 
-//This is a function that returns the circumference of a circle
+//Returns the circumference of a circle
 var circumference = function(circle){
     return 2*Math.PI*circle.radius;
 };
 //define circle object
-var circle = {x:100,y:200,radius:50};
+var circle = {x:100,y:100,radius:50};
 
 //invoke the function
 console.log(circumference(circle)); //314.159
