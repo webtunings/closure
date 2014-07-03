62269c6586bb9ffaa737dc7c50d2b11c3191893e
webtunings <webtunings@gmail.com>
2014-07-03 22:36:25 +0530

    code

--- 1.2.1 Rule 1.js
+++ 1.2.1 Rule 1.js
@@ -1,6 +1,6 @@
 // Listing 1.4 - Variable declarations are hoisted at the top of enclosing function
 //This function tries to check the presence of variable "a" before and after its declaration
-function scopeTest(){
+function scopeTest() {
     console.log(a); // undefined - this means variable a is hoisted at this point.No ReferenceError
     var a = 1;
     console.log(a); //1
