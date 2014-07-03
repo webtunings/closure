8102b9d49e84b9cee4eb8c7e343f9d526c0b51ab
webtunings <webtunings@gmail.com>
2014-07-04 00:20:13 +0530

    code

--- 1.2.4 Rule 4.js
+++ 1.2.4 Rule 4.js
@@ -1,3 +1,21 @@
-/**
- * Created by pk on 4/7/14.
- */
+//Listing 1.6 Function definition expression hoisting rule
+
+//This function illustrates the hoisting of function definition expression
+function outer() {
+    //function inner is not in scope here
+    //it can not be invoked at this place
+    inner(); //typeError
+
+    console.log(typeof inner === "function");
+    //false
+
+    var inner = function () { console.log("I am not hoisted at the top");}
+
+    //function inner is in scope here
+    // it could be invoked at this place
+    inner();
+    console.log(typeof inner === "function");
+    //true
+}
+
+outer();
