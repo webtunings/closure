ac3207996a4d4ea24f224b2694ab4ad69da75b13
webtunings <webtunings@gmail.com>
2014-07-04 00:13:43 +0530

    code

---
+++ 1.2.3 Rule 3.js
@@ -0,0 +1,21 @@
+//Listing 1.5 Function declaration statement hoisting rule
+
+//This function illustrates the hoisting of function statement
+function outer() {
+    //function inner is in scope here
+    //it could be invoked at this place
+    inner();
+
+    console.log(typeof inner === "function");
+    //true
+
+    function inner() { console.log("I can be invoked anywhere inside outer function");}
+
+    //function inner is in scope here
+    // it could be invoked at this place
+    inner();
+    console.log(typeof inner === "function");
+    //true
+}
+
+outer();
