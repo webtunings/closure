10bfff29a6f2c55dce2f77b5a24c94363be6edb3
webtunings <webtunings@gmail.com>
2014-07-04 23:15:16 +0530

    code

--- 1.3.1 Function invocation.js
+++ 1.3.1 Function invocation.js
@@ -1,7 +1,7 @@
 //Listing 1.7 Function invocation
 
 //This function returns the word boundaries of a string
-//\w to \W or \W to \b transition is a word boundary
+//\w to \W or \W to \w transition is a word boundary
 function wordBoundaries(subject) {
     //regex for word boundary position
     var pattern = /\b/g;
