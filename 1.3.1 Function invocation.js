00e5e658f9451244ea2e964f30a4f065a45b4b71
webtunings <webtunings@gmail.com>
2014-07-04 23:11:55 +0530

    code

---
+++ 1.3.1 Function invocation.js
@@ -0,0 +1,14 @@
+//Listing 1.7 Function invocation
+
+//This function return the word boundaries of a string
+//\w to \W or \W to \b transition is a word boundary
+function wordBoundaries(subject) {
+    //regex for word boundary position
+    var pattern = /\b/g;
+
+    //invoke match method defined on the string
+    return subject.match(pattern).length;
+}
+
+var book = "JavaScript Closure in Depth";
+console.log(wordBoundaries(book)); //8
