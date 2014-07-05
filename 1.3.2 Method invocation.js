1a115e9f2e9de6b98c522b93072284b034e12d5a
webtunings <webtunings@gmail.com>
2014-07-05 13:53:28 +0530

    code

--- 1.3.2 Method invocation.js
+++ 1.3.2 Method invocation.js
@@ -6,16 +6,17 @@ var cylinder = {
     height: 20
 };
 
-//define function on cylinder object
-//volume is the property of cylinder object
-//this inside function is the cylinder object
-//this.radius means radius property of the cylinder object
+/*
+define function on cylinder object
+volume is the property of cylinder object
+this inside function is the cylinder object
+this.radius means radius property of the cylinder object
+this = invocation context = cylinder object
+*/
 cylinder.volume = function() {
     return Math.PI * Math.pow(this.radius,2) * this.height;
 };
 
 //invoke the method on the cylinder object
 console.log(cylinder.volume());
-
-
 
