a56612ec7391037778f4e0bdbe0ba90863377235
webtunings <webtunings@gmail.com>
2014-07-06 00:59:02 +0530

    code

--- 1.3.4.2 Apply method.js
+++ 1.3.4.2 Apply method.js
@@ -1,5 +1,8 @@
 //Listing 1.12 - call & apply with arguments
+
+//this function makes all arguments non enumerable
 var makeNonEnumerable = function(){
+    //iterate over all arguments and change the enumerable attribute false
     for(var i=0; i < arguments.length; i++){
         Object.defineProperty(this,arguments[i],{enumerable:false});
     }
@@ -7,12 +10,16 @@ var makeNonEnumerable = function(){
 
 var testObject1 = {x:1,y:2,z:3};
 
+//make x and y property non enumerable
+//we pass individual argument instead of array
 makeNonEnumerable.call(testObject1,"x","y");
+//check the enumerable attribute
 console.log(Object.getOwnPropertyDescriptor(testObject1,"x").enumerable); //false
 console.log(Object.getOwnPropertyDescriptor(testObject1,"y").enumerable); //false
 console.log(Object.getOwnPropertyDescriptor(testObject1,"z").enumerable); //true
 
 var testObject2 = {p:1,q:2,r:3};
+//we pass array instead of individual argument
 makeNonEnumerable.apply(testObject2,["p","q"]);
 console.log(Object.getOwnPropertyDescriptor(testObject2,"p").enumerable); //false
 console.log(Object.getOwnPropertyDescriptor(testObject2,"q").enumerable); //false
