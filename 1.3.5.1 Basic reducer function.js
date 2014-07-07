d141ed164bbc40fc62590e6c7628674ecf662312
webtunings <webtunings@gmail.com>
2014-07-08 01:08:03 +0530

    code

--- 1.3.5.1 Basic reducer function.js
+++ 1.3.5.1 Basic reducer function.js
@@ -1,5 +1,12 @@
 //Listing 1.14 Basic reducer function
 
+//reducer object has one array a and method reduce
+//reduce does the job of reducing using reduce method 0.5*(x+y)
+//Below is the calculation using reduce method and array [100,200,300]
+//x=100, y=200
+//0.5*(100 + 200) = 150 -> this will become x in next iteration
+//x=150, y=300
+//0.5*(150 + 300) = 225
 var reducer = {
     a: [100,200,300],
     reduce: function() {
@@ -8,5 +15,5 @@ var reducer = {
         });
     }
 };
-console.log(reducer.reduce()); //NaN
+console.log(reducer.reduce()); //225
 
