09fae69e4eb184386c0f2b622e8b3664b2bbeb56
webtunings <webtunings@gmail.com>
2014-07-10 01:38:57 +0530

    code

--- 1.3.4.2 Apply method.js
+++ 1.3.4.2 Apply method.js
@@ -13,17 +13,17 @@ var testObject1 = {x:1,y:2,z:3};
 //make x and y property non enumerable
 //We pass individual argument instead of array
 makeNonEnumerable.call(testObject1,"x","y");
-//check the enumerable attribute
-console.log(Object.getOwnPropertyDescriptor(testObject1,"x").enumerable); //false
-console.log(Object.getOwnPropertyDescriptor(testObject1,"y").enumerable); //false
-console.log(Object.getOwnPropertyDescriptor(testObject1,"z").enumerable); //true
+//check the enumerable attribute by console.log
+Object.getOwnPropertyDescriptor(testObject1,"x").enumerable; //false
+Object.getOwnPropertyDescriptor(testObject1,"y").enumerable; //false
+Object.getOwnPropertyDescriptor(testObject1,"z").enumerable; //true
 
 var testObject2 = {p:1,q:2,r:3};
 //We pass array instead of individual argument
 makeNonEnumerable.apply(testObject2,["p","q"]);
-console.log(Object.getOwnPropertyDescriptor(testObject2,"p").enumerable); //false
-console.log(Object.getOwnPropertyDescriptor(testObject2,"q").enumerable); //false
-console.log(Object.getOwnPropertyDescriptor(testObject2,"r").enumerable); //true
+Object.getOwnPropertyDescriptor(testObject2,"p").enumerable; //false
+Object.getOwnPropertyDescriptor(testObject2,"q").enumerable; //false
+Object.getOwnPropertyDescriptor(testObject2,"r").enumerable; //true
 
 var property;
 for(property in testObject1){
