27793e5c57eff8af03889709dcad6037c6d0d19c
webtunings <webtunings@gmail.com>
2014-07-13 20:14:03 +0530

    code

--- 4.1_2 arguments object.js
+++ 4.1_2 arguments object.js
@@ -4,6 +4,7 @@ function add(){
     console.log(arguments.length); //3
     var sum = 0;
     //iterate over all arguments
+    //trick - save arguments.length in some variable
     for(var i=0; i < arguments.length; i++){
         sum +=arguments[i];
     }
